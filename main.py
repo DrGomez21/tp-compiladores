128591bd0dfd3e6adf8efdaffa68233a6482972b
DrGomez21 <devgo.bydrgm21@gmail.com>
2024-04-07 16:21:01 -0400

    Cambios menores

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import re
+import tabla_de_simbolos as simbolos
 
 def main():
-    archivo = open("fuente.txt", 'r')  
+    archivo = open("entrada.txt", 'r')  
     contenido = archivo.readlines() # Leer las lineas del archivo.
+
+    bool_espacio = '\n' or ' ' or '\t'
+    for linea in contenido:
+        for letra in linea:
+            if letra not in bool_espacio:
+                simbolos.get_comp_lex(letra)
+
+
+    print(contenido)
 
 if __name__ == "__main__":
     main()
