25101c49bf341d3ab0c758558a8058b2fc6b03b4
jazgamarra <j444min@gmail.com>
2024-04-07 15:54:18 -0400

    Crear tabla de simbolos :D

---
+++ tabla_de_simbolos.py
@@ -0,0 +1,21 @@
+def get_comp_lex(lexema): 
+    ''' buscar un lexema y retorna el componente lexico '''
+    return tabla_de_simbolos[lexema]
+
+
+# diccionario con simbolos iniciales 
+tabla_de_simbolos = {
+    "[": "L_CORCHETE",
+    "]": "R_CORCHETE",
+    "{": "L_LLAVE",
+    "}": "R_LLAVE",
+    ",": "COMA",
+    ":": "DOS_PUNTOS",
+    '"': "LITERAL_CADENA",
+    "number": "LITERAL_NUM",
+    "true": "PR_TRUE", # puede ser mayuscula o minuscula
+    "false": "PR_FALSE", # puede ser mayuscula o minuscula
+    "null": "PR_NULL",
+    "eof": "EOF"
+}
+ 
